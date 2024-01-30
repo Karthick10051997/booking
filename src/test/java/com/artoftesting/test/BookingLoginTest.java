fd8f1c049572000368dfbd00b301ea95b970de9c
Karthick10051997 <155796939+Karthick10051997@users.noreply.github.com>
2024-01-30 17:21:15 +0530

    test

--- src/test/java/com/artoftesting/test/BookingLoginTest.java
+++ src/test/java/com/artoftesting/test/BookingLoginTest.java
@@ -2,10 +2,11 @@ package com.artoftesting.test;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-
+import org.testng.Reporter;
 import org.testng.annotations.Test;
 
 import com.artoftesting.base.TestBase;
+import com.artoftesting.base.TestDetail;
 import com.artoftesting.pages.BookingPage;
 import com.artoftesting.pages.LoginPage;
 
@@ -15,28 +16,37 @@ public class BookingLoginTest extends TestBase {
 //	LoginPage loginPage;
 	BookingPage bookingpage;
 	//Log4j configuration
-	private static final Logger log = LogManager.getLogger(SauceDemoLoginTest.class);
 	
-	@Test(testName = "Calculate Booking service")
-	public void loginTest() throws InterruptedException{
-		
-		log.info("Verifying successful login.");
-		
+	
+	@TestDetail(testCaseID ="TC_001",
+			testCaseName ="Booking_Register_Mail",
+			author ="karthick",
+			    module ="Booking",
+			    category = "sanity"
+			)
+	@Test(groups ="sanity")
+	
+	public void bookingservice() throws InterruptedException
+	{
+		System.out.println("testlogin");
+		Screenshot("Login");
 		LoginPage loginPage = new LoginPage(driver);
 		loginPage.enterUsername();
 		loginPage.enterPassword();
 		loginPage.clickSignIn();
+		Screenshot("Login successful");
 		bookingpage = new BookingPage(driver);
 		bookingpage.clickBookingModuleTab();
 		bookingpage.clickBookingModuleOption();
 		bookingpage.clickMailBookingOption();
 		bookingpage.clickDomesticMailBookingOption();
-		bookingpage.enterOriginPincode("600019");
+		//bookingpage.enterOriginPincode("600019");
 		bookingpage.enterDestinationPincode("600119");
 		bookingpage.enterPhysicalWeight("100");
 		bookingpage.clickSearchServicesButton();
 		bookingpage.VerifyAvailableServices();
-		
-		
+		Screenshot("Validating");
 	}
+	
+
 }
